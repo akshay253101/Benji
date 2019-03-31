93c9b78d96538f13cecb726b41eb73f02c1dfdff
Akshay <akshay253101yadav@gmail.com>
2019-04-01 01:29:16 +0530

    modified package name

--- app/src/main/java/com/example/beetlestance/benji/di/modules/viewModelModule/ViewModelBindingModule.kt
+++ app/src/main/java/com/example/beetlestance/benji/di/modules/viewModelModule/ViewModelBindingModule.kt
@@ -4,10 +4,6 @@ import androidx.lifecycle.ViewModelProvider
 import dagger.Binds
 import dagger.Module
 
-/**
- * Module used to define the connection between the framework's [ViewModelProvider.Factory] and
- * our own implementation: [DaggerAwareViewModelFactory].
- */
 
 @Module
 abstract class ViewModelBindingModule {
