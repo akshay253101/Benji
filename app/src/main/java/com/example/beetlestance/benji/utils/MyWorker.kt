5412539b31a228646344864b6273bf6a3acfbb12
Akshay <akshay253101yadav@gmail.com>
2019-05-24 20:53:24 +0530

    Firebase Push notification support added

---
+++ app/src/main/java/com/example/beetlestance/benji/utils/MyWorker.kt
@@ -0,0 +1,12 @@
+package com.example.beetlestance.benji.utils
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class MyWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
+
+    override fun doWork(): Result {
+       return Result.success()
+    }
+}
